f34b81368e000fcea9348dfc7d24b043d8a0e709
Gokul <gokulchandan18@gmail.com>
2021-12-03 14:22:30 +0530

    commit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../Shared/ingredients.module';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+ingredients: Ingredient[] =[
+  new Ingredient('Apples',5),
+  new Ingredient('Tomatoes',10)
 
+];
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
